b275f437c56467757edbe88b75a7d2366dfe0c40
bhkolli <bhkolli@BHKOLLI-LCHMG.partnet.cisco.com>
2019-11-08 15:10:18 +0530

    spring boot Gradle project

--- challenge/src/main/java/com/db/awmd/challenge/service/EmailNotificationService.java
+++ challenge/src/main/java/com/db/awmd/challenge/service/EmailNotificationService.java
@@ -4,13 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.db.awmd.challenge.domain.Account;
-import com.db.awmd.challenge.web.AccountsController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class EmailNotificationService implements NotificationService {
-	private static final Logger log = LoggerFactory.getLogger(AccountsController.class);
+	private static final Logger log = LoggerFactory.getLogger(EmailNotificationService.class);
   @Override
   public void notifyAboutTransfer(Account account, String transferDescription) {
     //THIS METHOD SHOULD NOT BE CHANGED - ASSUME YOUR COLLEAGUE WILL IMPLEMENT IT
