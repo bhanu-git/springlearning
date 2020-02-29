1932b8cc7c182c2b9d35d028392ef62fb8c196f3
bhkolli <bhkolli@BHKOLLI-LCHMG.partnet.cisco.com>
2020-02-29 19:35:23 +0530

    Spring boot + Basic Auth Logic

---
+++ HelloWorldAngularSpringBootApplication/src/main/java/com/sb/security/basic/auth/AuthenticationBean.java
@@ -0,0 +1,18 @@
+package com.sb.security.basic.auth;
+
+public class AuthenticationBean {
+
+	String message;
+	
+	
+	public AuthenticationBean(String message) {
+		// TODO Auto-generated constructor stub
+		this.message = message;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
